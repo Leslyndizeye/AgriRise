d5822c158d715ef417e040f34447b3fddcc7e0d6
Leslyndizeye <l.ndizeye@alustudent.com>
2024-11-24 23:33:09 +0400

    back-end failed me

--- front-end/vite-project/src/pages/Home.jsx
+++ front-end/vite-project/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from '../components/Hero';
 import Popular from '../components/Popular';
-import Offer from '../components/Offer.jsx';
+import Offer from '../components/Offer';
 import NewCollections from '../components/NewCollections';
 import NewsLetter from '../components/NewsLetter';
 
